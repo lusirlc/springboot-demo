4a96bcfa7238ae1df5dcfb8823798426084f3802
luchuan <luchuan365@126.com>
2018-12-28 21:23:46 +0800

    create a springboot start demo

---
+++ springboot_start/src/main/java/cn/ulegal/springboot/SpringbootApplication.java
@@ -0,0 +1,16 @@
+package cn.ulegal.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+	void method1(){
+		System.out.println("method1");
+	}
+}
+
